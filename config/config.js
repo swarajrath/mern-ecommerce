5b2125a0b87a56a6c3f6236766fb530992fe90f7
Swaraj Rath <gulal009@gmail.com>
2020-11-30 21:51:32 +0100

    change configuration

--- config/config.js
+++ config/config.js
@@ -2,11 +2,7 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: process.env.MONGODB_URI ||
-    process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' +
-    (process.env.MONGO_PORT || '27017') +
-    '/mernproject',
+  mongoUri: 'mongodb+srv://taskapp:d3mydreamworld@cluster0.nu9xh.mongodb.net/mernecommerce?authSource=admin&replicaSet=atlas-itjizv-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true',
   stripe_connect_test_client_id: 'YOUR_stripe_connect_test_client',
   stripe_test_secret_key: 'YOUR_stripe_test_secret_key',
   stripe_test_api_key: 'YOUR_stripe_test_api_key'
